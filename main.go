caadc00f2bd1b0b08a591cd9ca2b94a96dbda8b9
程坤酱 <chengkun0804@gmail.com>
2021-10-14 22:47:52 +0800

    gin hello

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-  fmt.Printf("Hello world\n")
+	fmt.Printf("Hello world\n")
+	r := gin.Default()
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{"message": "pong ck!"})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
